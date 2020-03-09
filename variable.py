28692d919c5408a321546aae32b453e4bd61f3a1
addiefancensie <addiefancensie@gmail.com>
2020-03-09 02:14:49 -0700

    variable commit

---
+++ variable.py
@@ -0,0 +1,28 @@
+#this ia an illustration of how to use the word print and display text
+print("good morning")
+#I basically just displayed what i did this weekend
+print("I didn't do much this weekend , i literally just spent my weekend w my niece - babysitting the rascal")
+#displaying the name of the book i finished reading
+print("I finally finished reading the book I've been reading for the past few days , called 'The light between oceans'")
+#declaring the variable intHours as 40
+intHours = ("40")
+#declaring the variable price as 55.6
+price = ("55.6")
+#declaring the variable fav_colour as gold
+fav_colour = ("gold")
+#declaring the variable p as 3
+p =("3")
+print(intHours)
+print(price)
+print(fav_colour)
+print(p)
+str_truth ="""I am learning python basic learning.
+It's cool to learn a new language.
+But hey the weekend was fun."""
+print(str_truth)
+print(4+6)
+print("4+6")
+pi = (3.14)
+r = (5)
+A = pi*r**2
+print(A)
